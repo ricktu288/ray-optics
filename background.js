8637b2b8e83e4f4f1484db90bb2255acf76a36af
chengscott <chengscott@live.com>
2016-05-17 22:35:07 +0800

    js lint

--- background.js
+++ background.js
@@ -1,5 +1,13 @@
 chrome.app.runtime.onLaunched.addListener(function(launchData) {
-  chrome.app.window.create('light.html', {bounds: {width: 1000, height: 600}, state:"normal", minWidth: 950, minHeight: 250}, function(win) {
-    
+  chrome.app.window.create('light.html', {
+    bounds: {
+      width: 1000,
+      height: 600
+    },
+    state: 'normal',
+    minWidth: 950,
+    minHeight: 250
+  }, function(win) {
+
   });
 });
