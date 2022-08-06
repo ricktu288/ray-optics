a86c4be7f18dd100e78f54584088621d643dcb8e
Yi-Ting Tu <ricktu256@gmail.com>
2022-08-05 21:00:03 -0400

    Change color of Led to match that of the original point source.

--- simulator/index.js
+++ simulator/index.js
@@ -1726,8 +1726,9 @@ var canvasPainter = {
   //=================================將物件畫到Canvas上====================================
   draw: function(obj, canvas) {
   //var ctx = canvas.getContext('2d');
-  ctx.fillStyle = getMouseStyle(obj, 'rgb(255,0,0)');
+  ctx.fillStyle = getMouseStyle(obj, 'rgb(0,255,0)');
   ctx.fillRect(obj.p1.x - 2, obj.p1.y - 2, 5, 5);
+  ctx.fillStyle = getMouseStyle(obj, 'rgb(255,0,0)');
   ctx.fillRect(obj.p2.x - 2, obj.p2.y - 2, 3, 3);
   },
 
