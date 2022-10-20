be56bf0c7c39fc27f417e9611921509ebd1e06ab
StasFainer <114682673+StasFainer@users.noreply.github.com>
2022-10-21 01:25:38 +0300

    Added "fresnel-lens.json" and "rochester-cloak.json" to samples

--- simulator/js/index.js
+++ simulator/js/index.js
@@ -367,7 +367,8 @@ window.onload = function(e) {
                   "convex-lens.json", "concave-lens.json", "spherical-aberration.json", "zoom-lens.json",
                   "apparent-depth-of-an-object-under-water.json", "compound-microscope.json", "images-formed-by-two-mirrors.json",
                   "reflection-and-refraction-of-a-single-ray.json", "spherical-lens-and-mirror.json", "chromatic-dispersion.json",
-                  "bended-pencil.json", "broken-pencil.json", "NL-simulation.json", "luneburg-lens.json", "maxwell-fisheye-lens.json", "inferior-mirage.json" ];
+                  "bended-pencil.json", "broken-pencil.json", "NL-simulation.json", "luneburg-lens.json", "maxwell-fisheye-lens.json", "inferior-mirage.json",
+                  "fresnel-lens.json", "rochester-cloak.json" ];
   for (i = 1; ; i++) {
     var elt = document.getElementById("sample" + i);
     if (!elt) break;
