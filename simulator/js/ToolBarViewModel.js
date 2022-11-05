17210e1281857bdacc25f607c8b5f2dde68e84e0
StasFainer <114682673+StasFainer@users.noreply.github.com>
2022-11-05 18:22:46 +0200

    Changed "sampleList" size from 28 to 29

--- simulator/js/ToolBarViewModel.js
+++ simulator/js/ToolBarViewModel.js
@@ -1,6 +1,6 @@
 var i;
 var sampleList = [];
-for (i = 1; i <= 28; i++)
+for (i = 1; i <= 29; i++)
   sampleList.push(new ToolBarItem("Sample " + i, "sample" + i, undefined, ToolTypeEnum.BUTTON, undefined, undefined));
 
 function ToolBarViewModel() {
