79efe456079ebd7e252a16cebb4c5b6c5aaaea81
Luke Cheng <luke.cheng.725443@gmail.com>
2022-12-31 11:08:53 +0800

    Create de.js

---
+++ simulator/locales/de.js
@@ -0,0 +1,622 @@
+if (typeof locales == 'undefined') locales = {};
+locales["de"] = {
+  "appName": {
+    "message": "Strahl Optik Simulation"
+  },
+  "appShortName": {
+    "incomplete": true,
+    "message": "Ray Optics"
+  },
+  "appDesc": {
+    "incomplete": true,
+    "message": "A free, open-source web app for creating and simulating 2D geometric optical scenes."
+  },
+  "launch_simulator": {
+    "incomplete": true,
+    "message": "Launch Simulator"
+  },
+  "home": {
+    "incomplete": true,
+    "message": "Home"
+  },
+  "gallery": {
+    "incomplete": true,
+    "message": "Gallery"
+  },
+  "welcome": {
+    "incomplete": true,
+    "message": "<span style=\"font-size:22pt\">Welcome to Ray Optics Simulation</span><br>To add an optical component, select a tool and click the blank space.<br>To load an example, please <a href=\"https://phydemo.app/ray-optics/gallery/\">go to the Gallery page</a>."
+  },
+  "toolbar_title": {
+    "incomplete": true,
+    "message": "Tool:&nbsp;"
+  },
+  "toolname_laser": {
+    "incomplete": true,
+    "message": "Single ray"
+  },
+  "toolname_led": {
+    "incomplete": true,
+    "message": "Point source"
+  },
+  "toolname_radiant": {
+    "incomplete": true,
+    "message": "Point source"
+  },
+  "toolname_parallel": {
+    "incomplete": true,
+    "message": "Beam"
+  },
+  "toolname_mirror_": {
+    "incomplete": true,
+    "message": "Mirror"
+  },
+  "toolname_mirror": {
+    "incomplete": true,
+    "message": "Mirror"
+  },
+  "toolname_arcmirror": {
+    "incomplete": true,
+    "message": "Mirror"
+  },
+  "toolname_parabolicmirror": {
+    "incomplete": true,
+    "message": "Mirror"
+  },
+  "toolname_curvedmirror": {
+    "incomplete": true,
+    "message": "Mirror"
+  },
+  "toolname_idealmirror": {
+    "incomplete": true,
+    "message": "Ideal curved mirror"
+  },
+  "toolname_refractor_": {
+    "incomplete": true,
+    "message": "Glass"
+  },
+  "toolname_blocker_": {
+    "incomplete": true,
+    "message": "Blocker"
+  },
+  "toolname_beamsplitter": {
+    "incomplete": true,
+    "message": "Beam Splitter"
+  },
+  "toolname_halfplane": {
+    "incomplete": true,
+    "message": "Glass"
+  },
+  "toolname_circlelens": {
+    "incomplete": true,
+    "message": "Glass"
+  },
+  "toolname_refractor": {
+    "incomplete": true,
+    "message": "Glass"
+  },
+  "toolname_lens": {
+    "incomplete": true,
+    "message": "Ideal lens"
+  },
+  "toolname_sphericallens": {
+    "incomplete": true,
+    "message": "Spherical lens"
+  },
+  "toolname_curvedglass": {
+    "incomplete": true,
+    "message": "Glass"
+  },
+  "toolname_blackline": {
+    "incomplete": true,
+    "message": "Line Blocker"
+  },
+  "toolname_blackcircle": {
+    "incomplete": true,
+    "message": "Circle Blocker"
+  },
+  "toolname_ruler": {
+    "incomplete": true,
+    "message": "Ruler"
+  },
+  "toolname_protractor": {
+    "incomplete": true,
+    "message": "Protractor"
+  },
+  "tool_power": {
+    "incomplete": true,
+    "message": "Detector"
+  },
+  "toolname_power": {
+    "incomplete": true,
+    "message": "Detector"
+  },
+  "toolname_text": {
+    "incomplete": true,
+    "message": "Text"
+  },
+  "toolname_": {
+    "incomplete": true,
+    "message": "Move view"
+  },
+  "modebar_title": {
+    "incomplete": true,
+    "message": "View:"
+  },
+  "modename_light": {
+    "incomplete": true,
+    "message": "Rays"
+  },
+  "modename_extended_light": {
+    "incomplete": true,
+    "message": "Extended rays"
+  },
+  "modename_images": {
+    "incomplete": true,
+    "message": "All Images"
+  },
+  "modename_observer": {
+    "incomplete": true,
+    "message": "Seen by observer"
+  },
+  "color_mode": {
+    "incomplete": true,
+    "message": "Simulate Colors"
+  },
+  "File: ": {
+    "incomplete": true,
+    "message": "File: "
+  },
+  "Tools: ": {
+    "incomplete": true,
+    "message": "Tools: "
+  },
+  "tool_laser": {
+    "incomplete": true,
+    "message": "Ray"
+  },
+  "tool_parallel": {
+    "incomplete": true,
+    "message": "Beam"
+  },
+  "tool_radiant": {
+    "incomplete": true,
+    "message": "360 degrees"
+  },
+  "tool_led": {
+    "incomplete": true,
+    "message": "Finite angle"
+  },
+  "tool_radiant_": {
+    "incomplete": true,
+    "message": "Point source"
+  },
+  "tool_mirror_": {
+    "incomplete": true,
+    "message": "Mirrors"
+  },
+  "tool_mirror": {
+    "incomplete": true,
+    "message": "Segment"
+  },
+  "tool_arcmirror": {
+    "incomplete": true,
+    "message": "Circular Arc"
+  },
+  "tool_parabolicmirror": {
+    "incomplete": true,
+    "message": "Parabolic"
+  },
+  "tool_curvedmirror": {
+    "incomplete": true,
+    "message": "Custom Equation"
+  },
+  "tool_beamsplitter": {
+    "incomplete": true,
+    "message": "Beam Splitter"
+  },
+  "tool_idealmirror": {
+    "incomplete": true,
+    "message": "Ideal Curved"
+  },
+  "tool_refractor_": {
+    "incomplete": true,
+    "message": "Glasses"
+  },
+  "tool_blocker_": {
+    "incomplete": true,
+    "message": "Blockers"
+  },
+  "tool_halfplane": {
+    "incomplete": true,
+    "message": "Half-plane"
+  },
+  "tool_circlelens": {
+    "incomplete": true,
+    "message": "Circle"
+  },
+  "tool_refractor": {
+    "incomplete": true,
+    "message": "Free-shape"
+  },
+  "tool_lens": {
+    "incomplete": true,
+    "message": "Ideal Lens"
+  },
+  "tool_sphericallens": {
+    "incomplete": true,
+    "message": "Spherical Lens"
+  },
+  "tool_curvedglass": {
+    "incomplete": true,
+    "message": "Custom Equation"
+  },
+  "tool_blackline": {
+    "incomplete": true,
+    "message": "Line Blocker"
+  },
+  "tool_blackcircle": {
+    "incomplete": true,
+    "message": "Circle Blocker"
+  },
+  "tool_ruler": {
+    "incomplete": true,
+    "message": "Ruler"
+  },
+  "tool_protractor": {
+    "incomplete": true,
+    "message": "Protractor"
+  },
+  "tool_text": {
+    "incomplete": true,
+    "message": "Text"
+  },
+  "tool_": {
+    "incomplete": true,
+    "message": "Move view"
+  },
+  "View: ": {
+    "incomplete": true,
+    "message": "View: "
+  },
+  "Settings: ": {
+    "incomplete": true,
+    "message": "Settings: "
+  },
+  "mode_light": {
+    "incomplete": true,
+    "message": "Rays"
+  },
+  "mode_extended_light": {
+    "incomplete": true,
+    "message": "Extended rays"
+  },
+  "mode_images": {
+    "incomplete": true,
+    "message": "All Images"
+  },
+  "mode_observer": {
+    "incomplete": true,
+    "message": "Seen by Observer"
+  },
+  "zoom": {
+    "incomplete": true,
+    "message": "Zoom"
+  },
+  "help": {
+    "incomplete": true,
+    "message": "Help"
+  },
+  "rayDensity": {
+    "incomplete": true,
+    "message": "Ray Density"
+  },
+  "raydensity": {
+    "incomplete": true,
+    "message": "Ray Density:"
+  },
+  "undo": {
+    "incomplete": true,
+    "message": "Undo"
+  },
+  "redo": {
+    "incomplete": true,
+    "message": "Redo"
+  },
+  "reset": {
+    "incomplete": true,
+    "message": "Reset"
+  },
+  "save": {
+    "incomplete": true,
+    "message": "Save"
+  },
+  "export_svg": {
+    "incomplete": true,
+    "message": "Export"
+  },
+  "view_gallery": {
+    "incomplete": true,
+    "message": "View Gallery"
+  },
+  "save_name": {
+    "incomplete": true,
+    "message": "Name:"
+  },
+  "save_cancel": {
+    "incomplete": true,
+    "message": "Cancel"
+  },
+  "save_description": {
+    "incomplete": true,
+    "message": "To share your work, you can <a href=\"https://github.com/ricktu288/ray-optics/blob/master/CONTRIBUTING.md#contributing-items-to-the-gallery\" target=\"_blank\">contribute a new item</a> to the <a href=\"https://phydemo.app/ray-optics/gallery/\" target=\"_blank\">Gallery</a>."
+  },
+  "open": {
+    "incomplete": true,
+    "message": "Open"
+  },
+  "lockobjs": {
+    "incomplete": true,
+    "message": "Lock Objects"
+  },
+  "showgrid": {
+    "incomplete": true,
+    "message": "Grid"
+  },
+  "grid": {
+    "incomplete": true,
+    "message": "Snap to Grid"
+  },
+  "snaptogrid": {
+    "incomplete": true,
+    "message": "Snap to Grid"
+  },
+  "brightness": {
+    "incomplete": true,
+    "message": "Brightness:"
+  },
+  "wavelength": {
+    "incomplete": true,
+    "message": "Wavelength (nm):"
+  },
+  "emissionangle": {
+    "incomplete": true,
+    "message": "Emission Angle (°):"
+  },
+  "symmetric": {
+    "incomplete": true,
+    "message": "Symmetric"
+  },
+  "focallength": {
+    "incomplete": true,
+    "message": "Focal length:"
+  },
+  "cartesiansign": {
+    "incomplete": true,
+    "message": "Cartesian Sign Convention"
+  },
+  "refractiveindex": {
+    "incomplete": true,
+    "message": "Refractive index:"
+  },
+  "refractiveindex_note": {
+    "incomplete": true,
+    "message": "(relative if inside another glass)"
+  },
+  "refractiveindex_note_popover": {
+    "incomplete": true,
+    "message": "If this object is to be simulated with (absolute) refractive index n₁, but is placed inside another glass with refractive index n₀, then you should set the refractive index to n₁/n₀ here. In more complicated cases that several glasses overlap together, the effective reflective index of the overlapping region equals the product of the reflective indices of individual ones."
+  },
+  "cauchycoeff": {
+    "incomplete": true,
+    "message": "Cauchy coefficient"
+  },
+  "transmissionratio": {
+    "incomplete": true,
+    "message": "Transmission Ratio:"
+  },
+  "applytoall": {
+    "incomplete": true,
+    "message": "Apply to All"
+  },
+  "duplicate": {
+    "incomplete": true,
+    "message": "Duplicate"
+  },
+  "delete": {
+    "incomplete": true,
+    "message": "Delete"
+  },
+  "processing": {
+    "incomplete": true,
+    "message": "Processing... Click here to stop."
+  },
+  "restore": {
+    "incomplete": true,
+    "message": "Click here to restore your previous session.<br>⚠️ Important Change: This app will no longer store your session. Please save your work before closing this page."
+  },
+  "contribute": {
+    "incomplete": true,
+    "message": "Contribute"
+  },
+  "github": {
+    "incomplete": true,
+    "message": "GitHub Repo"
+  },
+  "about": {
+    "incomplete": true,
+    "message": "About"
+  },
+  "translate": {
+    "incomplete": true,
+    "message": "Help us translate this app"
+  },
+  "translated": {
+    "incomplete": true,
+    "message": "translated"
+  },
+  "undo_popover": {
+    "incomplete": true,
+    "message": "Undo last action. (Ctrl+Z)"
+  },
+  "redo_popover": {
+    "incomplete": true,
+    "message": "Redo last action. (Ctrl+Y)"
+  },
+  "reset_popover": {
+    "incomplete": true,
+    "message": "Reset all changes."
+  },
+  "save_popover": {
+    "incomplete": true,
+    "message": "Save the current file."
+  },
+  "open_popover": {
+    "incomplete": true,
+    "message": "Open an existing file or background image. The background image will be shown from the coordinate origin. (Or drop the file)"
+  },
+  "export_svg_popover": {
+    "incomplete": true,
+    "message": "Export to SVG."
+  },
+  "view_gallery_popover": {
+    "incomplete": true,
+    "message": "Go to the Gallery webpage to load an example."
+  },
+  "tool_laser_popover": {
+    "incomplete": true,
+    "message": "A single ray of light defined by two points. (Drag or Click to create. The first point is the source, shooting toward the second point.)"
+  },
+  "tool_parallel_popover": {
+    "incomplete": true,
+    "message": "A parallel beam of rays emerges from a line segment, with density controlled by the 'Ray density' slider. (Drag or click to create.)"
+  },
+  "tool_radiant_popover": {
+    "incomplete": true,
+    "message": "Rays emerge from a single point, with number controlled by the 'Ray density' slider. (Click to create.)"
+  },
+  "tool_led_popover": {
+    "incomplete": true,
+    "message": "Point source with a given emission angle."
+  },
+  "tool_mirror_popover": {
+    "incomplete": true,
+    "message": "Simulate the reflection of light on a mirror. (Drag or click to create.)"
+  },
+  "tool_arcmirror_popover": {
+    "incomplete": true,
+    "message": "A mirror whose shape is part of a circle, which is defined by three points. (Drag or click to create. The first 2 points define the gap between arc's edges, the last point defines the arc's size.)"
+  },
+  "tool_parabolicmirror_popover": {
+    "incomplete": true,
+    "message": "A mirror whose shape is a parabola, which is defined by three points. (Drag or click to create.)"
+  },
+  "tool_curvedmirror_popover": {
+    "incomplete": true,
+    "message": "A mirror whose shape is defined by a custom equation y = f(x), where (x,y) is in the transformed coordinates in which the two control points are (-1,0) and (1,0). (Drag or click to create.)"
+  },
+  "tool_beamsplitter_popover": {
+    "incomplete": true,
+    "message": "A mirror that transmits a percentage of incoming light. (Drag or click to create.)"
+  },
+  "tool_idealmirror_popover": {
+    "incomplete": true,
+    "message": "The idealized 'curved' mirror which obeys exactly the mirror equation (1/p + 1/q = 1/f). The focal length (in pixels) can be set directly. (Drag or click to create.)"
+  },
+  "tool_halfplane_popover": {
+    "incomplete": true,
+    "message": "Simulate the refraction and reflection of light on a surface. (Drag or click to create.)"
+  },
+  "tool_circlelens_popover": {
+    "incomplete": true,
+    "message": "Glass with a circle shape, defined by its center and a point on the surface. (Drag or click to create.)"
+  },
+  "tool_refractor_popover": {
+    "incomplete": true,
+    "message": "Glass with any shapes constructed from line segments and circular arcs. (Click to create a segment, drag to create an arc, click on the starting point when finish drawing.)"
+  },
+  "tool_lens_popover": {
+    "incomplete": true,
+    "message": "An ideal lens which obeys exactly the thin lens equation (1/p + 1/q = 1/f). The focal length (in pixels) can be set directly. (Drag or click to create.)"
+  },
+  "tool_sphericallens_popover": {
+    "incomplete": true,
+    "message": "A spherical lens. (Click to create.)"
+  },
+  "tool_curvedglass_popover": {
+    "incomplete": true,
+    "message": "Glass with the shape defined by a custom inequality f(x) &lt; y &lt; g(x), where (x,y) is in the transformed coordinates in which the two control points are (-1,0) and (1,0). (Drag or click to create.)"
+  },
+  "tool_blackline_popover": {
+    "incomplete": true,
+    "message": "A line-segment light blocker which absorbs the incident rays. (Drag or click to create.)"
+  },
+  "tool_blackcircle_popover": {
+    "incomplete": true,
+    "message": "A circle light blocker which absorbs the incident rays. (Drag or click to create.)"
+  },
+  "tool_ruler_popover": {
+    "incomplete": true,
+    "message": "A ruler from a point for zero and another point. The scale is in pixels. (Drag or click to create.)"
+  },
+  "tool_protractor_popover": {
+    "incomplete": true,
+    "message": "A protractor defined from the center and another point for the zero direction. The scale is in degrees. (Drag or click to create.)"
+  },
+  "tool_power_popover": {
+    "incomplete": true,
+    "message": "A tool for measuring the rate of energy flow (P), the rate of perpendicular momentum flow (F⊥), and the rate of parallel momentum flow (F∥) through a line segment. (Drag or click to create the line segment.) The units are arbitrary."
+  },
+  "tool_text_popover": {
+    "incomplete": true,
+    "message": "A text label."
+  },
+  "tool__popover": {
+    "incomplete": true,
+    "message": "Drag to move the view. (Mouse right button drag has the same function.)"
+  },
+  "mode_light_popover": {
+    "incomplete": true,
+    "message": "Show the rays. When the 'Ray density' is high, they appear to be continuous."
+  },
+  "mode_extended_light_popover": {
+    "incomplete": true,
+    "message": "Show both the rays and its extension. Orange indicates backward extensions, and gray indicates forward ones.<br>When 'Simulate Colors' is on, they are shown in source colors and distinguished by dashing instead."
+  },
+  "mode_images_popover": {
+    "incomplete": true,
+    "message": "Show the position of all images. Yellow points indicate real images, orange indicates virtual images, and gray (none in this picture) indicates virtual objects. Note that some images cannot be detected if 'Ray density' is not high enough.<br>When 'Simulate Colors' is on, they are shown in source colors and distinguished by dot sizes instead."
+  },
+  "mode_observer_popover": {
+    "incomplete": true,
+    "message": "Simulate the rays and images seen from some position. The blue circle is the observer. Any rays crossing it are considered to be 'observed'. The observer does not know where the rays actually begin, but may think they begin at some point(s) if they intersect there. The rays are shown in blue, and the points in yellow (real) or orange (virtual). (Drag the big blue dot to move the observer.)<br>When 'Simulate Colors' is on, they are shown in source colors instead."
+  },
+  "color_mode_popover": {
+    "incomplete": true,
+    "message": "<img src='../img/simulate_colors.png' align='left' style='margin-right: 10px; margin-bottom: 4px; max-width: 250px'>Simulate colors (wavelengths) of light sources, mixture of colors, and chromatic dispersion of glasses. You can set those parameters for those objects when selected. To simulate color spectra, overlap rays with different wavelengths. <br><b>⚠️ WARNING:<br><ul><li>The color of images may be very inaccurate, especially when Ray Density is high.</li><li>The 'wavelength' is only for color and dispersion. Wave optics phenomena are not simulated.</li><li>The 'Export' button does not work with color mixture.</ul></b></b>"
+  },
+  "rayDensity_popover": {
+    "incomplete": true,
+    "message": "Decides how dense should the lights be simulated. The number is in log scale."
+  },
+  "showgrid_popover": {
+    "incomplete": true,
+    "message": "Defines the visibility of the background grid."
+  },
+  "grid_popover": {
+    "incomplete": true,
+    "message": "Defines whether to snap to grid."
+  },
+  "lockobjs_popover": {
+    "incomplete": true,
+    "message": "Defines whether the objects can be moved or not."
+  },
+  "zoom_popover": {
+    "incomplete": true,
+    "message": "Zoom the view with percentage."
+  },
+  "help_popover": {
+    "incomplete": true,
+    "message": "<div style=\"font-weight: normal; font-size: 10pt;\"><b>Help popups</b><p>Click this button to toggle help popups for other buttons.</p><b>Constrained dragging</b><p>When dragging an object, hold Shift to limit to horizontal/vertical; hold Ctrl to clone. When creating a line-shaped object or dragging its endpoints, hold Shift to lock the angle or snap to 0°, 45°, 90°; hold Ctrl to rotate/scale about its center.</p><b>Group, rotate, and scale objects</b><p> Hold Ctrl, click several control points (e.g. endpoints), then click empty space to group these points and create a handle. Drag the handle (or press arrow keys) to move the group; Shift+drag to limit to horizontal/vertical; Ctrl+drag to rotate, Ctrl+Shift+drag to scale. Drag the \"x\" to set rotation/scaling center. Click the handle and press Ctrl+D to duplicate the group; press Delete to ungroup. </p><b>Edit coordinates directly</b><p>Double-click or right-click the control point (e.g. endpoint) of the object, edit the (x,y) coordinates in pixels, then press Enter.</p><b>Keyboard shortcuts</b><p>When an object is selected (by clicking it), press arrow keys to move; Ctrl+D to duplicate; Delete key to delete.</p></div>"
+  }
+}
