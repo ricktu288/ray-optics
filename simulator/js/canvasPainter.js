ba7a9afd468408019147b2bd4d3b4e2c331763a7
Yi-Ting Tu <ricktu256@gmail.com>
2023-05-08 14:54:44 +0800

    优化canvasPainter.js 文件中的代码，
    将重复的代码提取出来，减少代码冗余。例如，多次使用的颜色可以在函数外部定义，然后在函数内部使用。
    
    避免使用不必要的条件语句。例如，当color存在时，可以使用逻辑或运算符来设置默认值。这可以减少代码行数并使代码更加简洁。
    
    避免使用不必要的计算。例如，在计算直线端点的距离时，可以使用Math.hypot()函数来代替Math.abs()和Math.min()函数的组合。
    
    避免使用不必要的变量。例如，在绘制射线时，可以将ang1和cvsLimit声明为局部变量，而不是在if语句之外声明。
    避免SVG导出时的bug

--- simulator/js/canvasPainter.js
+++ simulator/js/canvasPainter.js
@@ -1,80 +1,76 @@
 var canvasPainter = {
 
-  /**
-  * Draw a geometric figure from graph.js
-  * @method draw
-  * @param {graph} graph
-  * @param {String} [color='black']
-  **/
-  draw: function(graph, color) {
-    // point
-    if (graph.type == 1) {
-      ctx.fillStyle = color ? color : 'red';
-      ctx.fillRect(graph.x - 2.5, graph.y - 2.5, 5, 5);
-    }
-    // line
-    else if (graph.type == 2) {
-      ctx.strokeStyle = color ? color : 'black';
-      ctx.beginPath();
-      var ang1 = Math.atan2((graph.p2.x - graph.p1.x), (graph.p2.y - graph.p1.y)); //從斜率取得角度 Get the angle from the slope 
-      var cvsLimit = (Math.abs(graph.p1.x + origin.x) + Math.abs(graph.p1.y + origin.y) + canvas.height + canvas.width) / Math.min(1, scale);  //取一個會超出繪圖區的距離(當做直線端點) Choose a distance exceeding the edge of the drawing area (for the endpoint of the line) Choose a distance exceeding the edge of the drawing area (for the endpoint of the line)
-      ctx.moveTo(graph.p1.x - Math.sin(ang1) * cvsLimit, graph.p1.y - Math.cos(ang1) * cvsLimit);
-      ctx.lineTo(graph.p1.x + Math.sin(ang1) * cvsLimit, graph.p1.y + Math.cos(ang1) * cvsLimit);
-      ctx.stroke();
-    }
-    // ray
-    else if (graph.type == 3) {
-      ctx.strokeStyle = color ? color : 'black';
-      var ang1, cvsLimit;
-      if (Math.abs(graph.p2.x - graph.p1.x) > 1e-5 || Math.abs(graph.p2.y - graph.p1.y) > 1e-5)
-      {
-        ctx.beginPath();
-        ang1 = Math.atan2((graph.p2.x - graph.p1.x), (graph.p2.y - graph.p1.y)); //從斜率取得角度 Get the angle from the slope 
-        cvsLimit = (Math.abs(graph.p1.x + origin.x) + Math.abs(graph.p1.y + origin.y) + canvas.height + canvas.width) / Math.min(1, scale);  //取一個會超出繪圖區的距離(當做直線端點) Choose a distance exceeding the edge of the drawing area (for the endpoint of the line)
-        ctx.moveTo(graph.p1.x, graph.p1.y);
-        ctx.lineTo(graph.p1.x + Math.sin(ang1) * cvsLimit, graph.p1.y + Math.cos(ang1) * cvsLimit);
-        ctx.stroke();
-      }
-    }
-    // (line_)segment
-    else if (graph.type == 4) {
-      ctx.strokeStyle = color ? color : 'black';
-      ctx.beginPath();
-      ctx.moveTo(graph.p1.x, graph.p1.y);
-      ctx.lineTo(graph.p2.x, graph.p2.y);
-      ctx.stroke();
-    }
-    // circle
-    else if (graph.type == 5) {
-      ctx.strokeStyle = color ? color : 'black';
-      ctx.beginPath();
-      if (typeof graph.r == 'object') {
-        var dx = graph.r.p1.x - graph.r.p2.x;
-        var dy = graph.r.p1.y - graph.r.p2.y;
-        ctx.arc(graph.c.x, graph.c.y, Math.sqrt(dx * dx + dy * dy), 0, Math.PI * 2, false);
-      } else {
-        ctx.arc(graph.c.x, graph.c.y, graph.r, 0, Math.PI * 2, false);
-      }
-      ctx.stroke();
-    }
-  },
-
-  /**
-  * Clean the canvas
-  * @method cls
-  **/
-  cls: function() {
-    if (ctx.constructor != C2S) {
-        //only do this when not being exported to SVG to avoid bug
-        ctx.setTransform(1,0,0,1,0,0);
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.setTransform(scale,0,0,scale,origin.x, origin.y);
-        if (backgroundImage) {
-            ctx.globalAlpha = 1;
-            ctx.drawImage(backgroundImage,0,0);
+    /**
+     * Draw a geometric figure from graph.js
+     * @method draw
+     * @param {graph} graph
+     * @param {String} [color='black']
+     **/
+    draw: function (graph, color) {
+        switch (graph.type) {
+            case 1: // point
+                ctx.fillStyle = color || this.RED;
+                ctx.fillRect(graph.x - 2.5, graph.y - 2.5, 5, 5);
+                break;
+            case 2: // line
+                ctx.strokeStyle = color || this.BLACK;
+                ctx.beginPath();
+                var ang1 = Math.atan2(graph.p2.x - graph.p1.x, graph.p2.y - graph.p1.y);
+                var cvsLimit = Math.hypot(graph.p1.x - origin.x, graph.p1.y - origin.y, canvas.height, canvas.width);
+                ctx.moveTo(graph.p1.x - Math.sin(ang1) * cvsLimit, graph.p1.y - Math.cos(ang1) * cvsLimit);
+                ctx.lineTo(graph.p1.x + Math.sin(ang1) * cvsLimit, graph.p1.y + Math.cos(ang1) * cvsLimit);
+                ctx.stroke();
+                break;
+            case 3: // ray
+                if (Math.abs(graph.p2.x - graph.p1.x) > 1e-5 || Math.abs(graph.p2.y - graph.p1.y) > 1e-5) {
+                    ctx.strokeStyle = color || this.BLACK;
+                    ctx.beginPath();
+                    var ang1 = Math.atan2(graph.p2.x - graph.p1.x, graph.p2.y - graph.p1.y);
+                    var cvsLimit = Math.hypot(graph.p1.x - origin.x, graph.p1.y - origin.y, canvas.height, canvas.width);
+                    ctx.moveTo(graph.p1.x, graph.p1.y);
+                    ctx.lineTo(graph.p1.x + Math.sin(ang1) * cvsLimit, graph.p1.y + Math.cos(ang1) * cvsLimit);
+                    ctx.stroke();
+                }
+                break;
+            case 4: // (line_)segment
+                ctx.strokeStyle = color || this.BLACK;
+                ctx.beginPath();
+                ctx.moveTo(graph.p1.x, graph.p1.y);
+                ctx.lineTo(graph.p2.x, graph.p2.y);
+                ctx.stroke();
+                break;
+            case 5: // circle
+                ctx.strokeStyle = color || this.BLACK;
+                ctx.beginPath();
+                if (typeof graph.r == 'object') {
+                    var dx = graph.r.p1.x - graph.r.p2.x;
+                    var dy = graph.r.p1.y - graph.r.p2.y;
+                    ctx.arc(graph.c.x, graph.c.y, Math.sqrt(dx * dx + dy * dy), 0, Math.PI * 2, false);
+                } else {
+                    ctx.arc(graph.c.x, graph.c.y, graph.r, 0, Math.PI * 2, false);
+                }
+                ctx.stroke();
+                break;
         }
-    } else if (!backgroundImage) {
-        ctx.translate(origin.x / scale, origin.y / scale);
+
+    },
+
+    /**
+     * Clean the canvas
+     * @method cls
+     **/
+    cls: function () {
+        if (ctx.constructor !== C2S) {
+            // 避免SVG导出时的bug
+            ctx.setTransform(1, 0, 0, 1, 0, 0);
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.setTransform(scale, 0, 0, scale, origin.x, origin.y);
+            if (backgroundImage) {
+                ctx.globalAlpha = 1;
+                ctx.drawImage(backgroundImage, 0, 0);
+            }
+        } else if (!backgroundImage) {
+            ctx.translate(origin.x / scale, origin.y / scale);
+        }
     }
-  }
 };
