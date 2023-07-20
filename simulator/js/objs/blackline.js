79dbd43f9a851eda0c3f944299629ae87f1c3da3
gljames24 <gljames24@gmail.com>
2023-07-20 15:29:25 -0600

    Added dichroic feature to black line

--- simulator/js/objs/blackline.js
+++ simulator/js/objs/blackline.js
@@ -13,7 +13,6 @@ objTypes['blackline'] = {
   move: objTypes['lineobj'].move,
   clicked: objTypes['lineobj'].clicked,
   dragging: objTypes['lineobj'].dragging,
-  rayIntersection: objTypes['lineobj'].rayIntersection,
 
   //將物件畫到Canvas上 Draw the obj on canvas
   draw: function(obj, canvas) {
@@ -26,6 +25,46 @@ objTypes['blackline'] = {
   ctx.lineTo(obj.p2.x, obj.p2.y);
   ctx.stroke();
   ctx.lineWidth = 1;
+  },
+
+  //顯示屬性方塊 Show the property box
+  p_box: function(obj, elem) {
+    this.dichroicSettings(obj,elem);
+  },
+
+  dichroicSettings: function(obj, elem){
+    if (colorMode && createAdvancedOptions(obj.isDichroic)) {
+      createBooleanAttr(getMsg('dichroic'), obj.isDichroic, function(obj, value) {
+          obj.isDichroic = value;
+          obj.wavelength = obj.wavelength || GREEN_WAVELENGTH;
+          obj.isDichroicFilter = obj.isDichroicFilter || false;
+          obj.bandwidth = obj.bandwidth || 10
+      }, elem);
+      createBooleanAttr(getMsg('filter'), obj.isDichroicFilter, function(obj, value) {
+        if(obj.isDichroic){
+          obj.isDichroicFilter = value;
+        }
+      }, elem);
+      createNumberAttr(getMsg('wavelength'), UV_WAVELENGTH, INFRARED_WAVELENGTH, 1, obj.wavelength || GREEN_WAVELENGTH, function(obj, value) { 
+        obj.wavelength = value;
+      }, elem);
+      createNumberAttr("± " + getMsg('bandwidth'), 0, (INFRARED_WAVELENGTH - UV_WAVELENGTH) , 1, obj.bandwidth || 10, function(obj, value) { 
+        obj.bandwidth = value;
+      }, elem);
+    }
+  },
+
+  wavelengthInteraction: function(blackline, ray){
+    var dichroicEnabled = colorMode && blackline.isDichroic && blackline.wavelength;
+    var rayHueMatchesMirror =  Math.abs(blackline.wavelength - ray.wavelength) <= (blackline.bandwidth || 0);
+    return !dichroicEnabled || (rayHueMatchesMirror != blackline.isDichroicFilter);
+  }, 
+
+  //Describes how the ray 
+  rayIntersection: function(blackline, ray) {
+    if (objTypes['blackline'].wavelengthInteraction(blackline,ray)) {
+      return objTypes['lineobj'].rayIntersection(blackline, ray);
+    }    
   },
 
   //當物件被光射到時 When the obj is shot by a ray
