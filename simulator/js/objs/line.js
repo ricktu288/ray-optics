024921b37559bd21952248c872e8bc1036eca3d1
Yi-Ting Tu <ricktu256@gmail.com>
2024-04-16 23:07:31 -0400

    WIP: Refactor `line` into a class

--- simulator/js/objs/line.js
+++ simulator/js/objs/line.js
@@ -1,55 +1,62 @@
-// Other -> Line / Arrow
-objTypes['line'] = {
+/**
+ * Line or arrow decoration
+ * Tools -> Other -> Line / Arrow
+ * @class
+ * @extends linearObjMixin(SceneObj)
+ * @property {Point} p1 - The first endpoint.
+ * @property {Point} p2 - The second endpoint.
+ * @property {boolean} arrow1 - Whether an arrow is pointing from the first endpoint.
+ * @property {boolean} arrow2 - Whether an arrow is pointing from the second endpoint.
+ */
+objTypes['line'] = class extends linearObjMixin(SceneObj) {
+  static type = 'line';
+  static defaultProperties = {
+    p1: null,
+    p2: null,
+    arrow1: false,
+    arrow2: false
+  };
 
-  // Create the obj
-  create: function (constructionPoint) {
-    return { type: 'line', p1: constructionPoint, p2: constructionPoint, arrow1: false, arrow2: false };
-  },
-
-  // Use the prototype lineobj
-  onConstructMouseDown: objTypes['lineobj'].onConstructMouseDown,
-  onConstructMouseMove: objTypes['lineobj'].onConstructMouseMove,
-  onConstructMouseUp: objTypes['lineobj'].onConstructMouseUp,
-  move: objTypes['lineobj'].move,
-  checkMouseOver: objTypes['lineobj'].checkMouseOver,
-  onDrag: objTypes['lineobj'].onDrag,
-
-  // Draw the obj on canvas
-  draw: function (obj, canvasRenderer, isAboveLight, isHovered) {
+  /**
+   * Draw an arrowhead at `p2`, pointing from `p1` to `p2`.
+   * @param {CanvasRenderer} canvasRenderer - The canvas renderer.
+   * @param {Point} p1
+   * @param {Point} p2
+   */
+  drawArrow(canvasRenderer, p1, p2) {
     const ctx = canvasRenderer.ctx;
-    ctx.strokeStyle = isHovered ? 'cyan' : ("white");
-    ctx.beginPath();
-    ctx.moveTo(obj.p1.x, obj.p1.y);
-    ctx.lineTo(obj.p2.x, obj.p2.y);
-    ctx.stroke();
-    if (obj.arrow1) {
-      this.drawArrow(ctx, obj.p1, obj.p2);
-    }
-    if (obj.arrow2) {
-      this.drawArrow(ctx, obj.p2, obj.p1);
-    }
-  },
-
-  // Draw the arrow
-  drawArrow: function (ctx, p1, p2) {
-    var angle = Math.atan2(p2.y - p1.y, p2.x - p1.x);
-    var len = 10;
+    const angle = Math.atan2(p2.y - p1.y, p2.x - p1.x);
+    const len = 10;
     ctx.beginPath();
     ctx.moveTo(p2.x, p2.y);
     ctx.lineTo(p2.x - len * Math.cos(angle - Math.PI / 6), p2.y - len * Math.sin(angle - Math.PI / 6));
     ctx.moveTo(p2.x, p2.y);
     ctx.lineTo(p2.x - len * Math.cos(angle + Math.PI / 6), p2.y - len * Math.sin(angle + Math.PI / 6));
     ctx.stroke();
-  },
+  }
 
-  // Show the property box
-  populateObjBar: function (obj, objBar) {
-    objBar.createBoolean(getMsg('arrow1'), obj.arrow1, function (obj, value) {
+  populateObjBar(objBar) {
+    objBar.createBoolean(getMsg('arrow1'), this.arrow1, function (obj, value) {
       obj.arrow1 = value;
     });
-    objBar.createBoolean(getMsg('arrow2'), obj.arrow2, function (obj, value) {
+
+    objBar.createBoolean(getMsg('arrow2'), this.arrow2, function (obj, value) {
       obj.arrow2 = value;
     });
   }
 
+  draw(canvasRenderer, isAboveLight, isHovered) {
+    const ctx = canvasRenderer.ctx;
+    ctx.strokeStyle = isHovered ? 'cyan' : 'white';
+    ctx.beginPath();
+    ctx.moveTo(this.p1.x, this.p1.y);
+    ctx.lineTo(this.p2.x, this.p2.y);
+    ctx.stroke();
+    if (this.arrow1) {
+      this.drawArrow(canvasRenderer, this.p1, this.p2);
+    }
+    if (this.arrow2) {
+      this.drawArrow(canvasRenderer, this.p2, this.p1);
+    }
+  }
 };
