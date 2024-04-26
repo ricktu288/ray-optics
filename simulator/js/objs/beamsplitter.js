5147c62832ba56d588bea099b560cff5da4a4ee3
Yi-Ting Tu <ricktu256@gmail.com>
2024-04-26 19:59:07 -0400

    WIP: Refactor `beamsplitter` into a class

--- simulator/js/objs/beamsplitter.js
+++ simulator/js/objs/beamsplitter.js
@@ -1,61 +1,68 @@
-// Mirror -> Beam Splitter
-objTypes['beamsplitter'] = {
+/**
+ * Beam splitter.
+ * Tools -> Mirror -> Beam splitter
+ * @property {Point} p1 - The first endpoint.
+ * @property {Point} p2 - The second endpoint.
+ * @property {number} p - The transmission ratio.
+ * @property {boolean} isDichroic - Whether it is a dichroic beam splitter.
+ * @property {boolean} isDichroicFilter - If true, the ray with wavelength outside the bandwidth interacts with the beam splitter. If false, the ray with wavelength inside the bandwidth interacts with the beam splitter.
+ * @property {number} wavelength - The target wavelength if dichroic is enabled. The unit is nm.
+ * @property {number} bandwidth - The bandwidth if dichroic is enabled. The unit is nm.
+ */
+objTypes['beamsplitter'] = class extends LinearObjMixin(BaseFilter) {
+  static type = 'beamsplitter';
+  static isOptical = true;
+  static defaultProperties = {
+    p1: null,
+    p2: null,
+    p: 0.5,
+    isDichroic: false,
+    isDichroicFilter: false,
+    wavelength: GREEN_WAVELENGTH,
+    bandwidth: 10
+  };
 
-  // Create the obj
-  create: function(constructionPoint) {
-    return {type: 'beamsplitter', p1: constructionPoint, p2: constructionPoint, p: .5};
-  },
-
-  dichroicSettings: objTypes['mirror'].dichroicSettings,
-
-  // Show the property box
-  populateObjBar: function (obj, objBar) {
-    objBar.createNumber(getMsg('transmissionratio'), 0, 1, 0.01, obj.p, function (obj, value) {
+  populateObjBar(objBar) {
+    objBar.createNumber(getMsg('transmissionratio'), 0, 1, 0.01, this.p, function (obj, value) {
       obj.p = value;
     });
-    dichroicSettings(obj, objBar);
-  },
 
-  // Use the prototype lineobj
-  onConstructMouseDown: objTypes['lineobj'].onConstructMouseDown,
-  onConstructMouseMove: objTypes['lineobj'].onConstructMouseMove,
-  onConstructMouseUp: objTypes['lineobj'].onConstructMouseUp,
-  move: objTypes['lineobj'].move,
-  checkMouseOver: objTypes['lineobj'].checkMouseOver,
-  onDrag: objTypes['lineobj'].onDrag,
-  checkRayIntersects: objTypes['lineobj'].checkRayIntersects,
+    super.populateObjBar(objBar);
+  }
 
-  // Draw the obj on canvas
-  draw: function (obj, canvasRenderer, isAboveLight, isHovered) {
+  draw(canvasRenderer, isAboveLight, isHovered) {
     const ctx = canvasRenderer.ctx;
     ctx.strokeStyle = isHovered ? 'cyan' : ('rgb(100,100,168)');
     ctx.beginPath();
-    ctx.moveTo(obj.p1.x, obj.p1.y);
-    ctx.lineTo(obj.p2.x, obj.p2.y);
+    ctx.moveTo(this.p1.x, this.p1.y);
+    ctx.lineTo(this.p2.x, this.p2.y);
     ctx.stroke();
-    ctx.strokeStyle = isHovered ? 'cyan' : ((scene.colorMode && obj.wavelength && obj.isDichroic) ? wavelengthToColor(obj.wavelength || GREEN_WAVELENGTH, 1) : 'rgb(100,100,168)');
+    ctx.strokeStyle = isHovered ? 'cyan' : ((scene.colorMode && this.wavelength && this.isDichroic) ? wavelengthToColor(this.wavelength || GREEN_WAVELENGTH, 1) : 'rgb(100,100,168)');
     ctx.setLineDash([15, 15]);
-    ctx.moveTo(obj.p1.x, obj.p1.y);
-    ctx.lineTo(obj.p2.x, obj.p2.y);
+    ctx.moveTo(this.p1.x, this.p1.y);
+    ctx.lineTo(this.p2.x, this.p2.y);
     ctx.stroke();
     ctx.setLineDash([]);
-  },
+  }
 
-  checkRayIntersects: function (obj, ray) {
-    return objTypes['mirror'].checkRayIntersects(obj, ray);
-  },
+  checkRayIntersects(ray) {
+    if (this.checkRayIntersectFilter(ray)) {
+      return this.checkRayIntersectsShape(ray);
+    } else {
+      return null;
+    }
+  }
 
-  // When the obj is shot by a ray
-  onRayIncident: function (obj, ray, rayIndex, incidentPoint) {
+  onRayIncident(ray, rayIndex, incidentPoint) {
     var rx = ray.p1.x - incidentPoint.x;
     var ry = ray.p1.y - incidentPoint.y;
 
     ray.p1 = incidentPoint;
-    var mx = obj.p2.x - obj.p1.x;
-    var my = obj.p2.y - obj.p1.y;
+    var mx = this.p2.x - this.p1.x;
+    var my = this.p2.y - this.p1.y;
     ray.p2 = geometry.point(incidentPoint.x + rx * (my * my - mx * mx) - 2 * ry * mx * my, incidentPoint.y + ry * (mx * mx - my * my) - 2 * rx * mx * my);
     var ray2 = geometry.line(incidentPoint, geometry.point(incidentPoint.x - rx, incidentPoint.y - ry));
-    var transmission = obj.p;
+    var transmission = this.p;
     ray2.brightness_s = transmission * ray.brightness_s;
     ray2.brightness_p = transmission * ray.brightness_p;
     ray2.wavelength = ray.wavelength;
@@ -71,5 +78,4 @@ objTypes['beamsplitter'] = {
       };
     }
   }
-
 };
