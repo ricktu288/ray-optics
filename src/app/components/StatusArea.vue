8ca7e07e9c01b31f266595c6b897311188088e98
Yi-Ting Tu <ricktu256@gmail.com>
2026-04-05 00:16:22 -0400

    Escape HTML in error/warning messages

--- src/app/components/StatusArea.vue
+++ src/app/components/StatusArea.vue
@@ -58,13 +58,13 @@
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 20">
         <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
       </svg>
-      <span v-html="warnings.join('<br>')"></span>
+      <span v-html="formattedWarningsHtml"></span>
     </div>
     <div id="error" v-show="errors.length > 0">
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-circle-fill" viewBox="0 0 16 20">
         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4m.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2"/>
       </svg>
-      <span v-html="errors.join('<br>')"></span>
+      <span v-html="formattedErrorsHtml"></span>
     </div>
     <VirtualKeyboard />
   </div>
@@ -84,6 +84,7 @@ import { app } from '../services/app'
 import VirtualKeyboard from './VirtualKeyboard.vue'
 import { vTooltipPopover } from '../directives/tooltip-popover'
 import { parseLinks } from '../utils/links.js'
+import escapeHtml from 'escape-html'
 
 export default {
   name: 'StatusArea',
@@ -156,6 +157,13 @@ export default {
       return `You are using the following alpha features:<ul>${listItems}</ul>Alpha features are still in development and may be incomplete or buggy.`
     })
 
+    const formattedWarningsHtml = computed(() =>
+      status.activeWarnings.value.map((line) => escapeHtml(String(line))).join('<br>')
+    )
+    const formattedErrorsHtml = computed(() =>
+      status.activeErrors.value.map((line) => escapeHtml(String(line))).join('<br>')
+    )
+
     return {
       showStatus: preferences.showStatus,
       notificationStyle,
@@ -171,6 +179,8 @@ export default {
       simulatorStatus: status.simulatorStatus,
       betaPopoverContent,
       alphaPopoverContent,
+      formattedWarningsHtml,
+      formattedErrorsHtml,
       // Methods
       handleForceStop
     }
